daf7de1f3e50a778f915a6bc41499fde5ea4afd6
Severin Field <sevdeawesome@gmail.com>
2021-12-03 22:44:02 -0800

    updated code

--- crossvalidation.py
+++ crossvalidation.py
@@ -1,15 +1,80 @@
 # k fold with k = # of rows in dataset
-import random
+
 import numpy
-import pandas 
+import math
+import pandas
+
 
 # load data into a dataframe
-data = pandas.read_fwf("small.txt",header=None) 
-# number of features
-num_features = data.iloc[0,1:].size
-num_instances = data.iloc[0:,0].size
+dataframe = pandas.read_fwf("large.txt",header=None) 
+# dataframe = dataframe.iloc[0:40, : ]
 
-for i in range(num_instances):
-    class_type = data.iloc[i, 0]
-    print("THE " + str(i) + "th object is in class: " + str(class_type))
+# number of features
+num_features = dataframe.iloc[0,1:].size
+num_instances = dataframe.iloc[0:,0].size
+
+def cross_validation_accuracy(data, current_set, feature_to_add):
+    this_set = current_set
+    # this_set.append(feature_to_add)
+    
+    for x in range(num_features):
+        y = x + 1
+        if y not in this_set and y != feature_to_add:
+            data[y].values[:] = 0
+    
+    # print(data)
+
+    num_correctly_classified = 0
+    for i in range(num_instances):
+        #label of curr instance
+        label_object_to_classify = data.iloc[i, 0]
+        #features of curr instance
+        object_to_classify = data.iloc[i, 1:]
+        
+        nearest_neighbor_distance = math.inf
+        nearest_neighbor_location = 0
+
+        for k in range(num_instances):
+            if k != i:
+                #find euclidian distance to object at k, k cannot be i
+                object_to_compare = data.iloc[k,1:]
+                summation = 0
+                for a in range(object_to_classify.size):
+                    b = a+1
+                    summation += ((object_to_classify.at[b] - object_to_compare.at[b]) **2)
+                distance = math.sqrt(summation)
+
+                if distance < nearest_neighbor_distance:
+                    nearest_neighbor_distance = distance
+                    nearest_neighbor_location = k
+                    nearest_neighbor_label = data.iloc[k,0]
+                # print(" ask if " + str(i) + " is nearest neighbor w/ " + str(k))
+        
+        
+        
+        print("Object " + str(i)  + " is class " + str(label_object_to_classify) + " and its nearest neighbor is " + str(nearest_neighbor_location) + " which is in class " + str(nearest_neighbor_label))
+        if(label_object_to_classify == nearest_neighbor_label): 
+            num_correctly_classified += 1
+    accuracy = num_correctly_classified / num_instances
+    return accuracy
+
+
+# # test function
+print(cross_validation_accuracy(dataframe.copy(), [], 1))
+print(dataframe)
+# print(cross_validation_accuracy(dataframe.copy(), [], 5))
+# # print(dataframe)
+# print(cross_validation_accuracy(dataframe.copy(), [], 4))
+# # for i in range(num_features):
+# #     print(i + 1)
+
+
+
+
+
+
+
+
+
+    
 
--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 import crossvalidation
-from crossvalidation import random
-from crossvalidation import num_features
+import random
+
 
 def accuracy():
     return(random.random())
@@ -10,22 +10,34 @@ def feature_search():
 
     current_set_of_features = []
 
-    for i in range(num_features):
+    for i in range(1, crossvalidation.num_features + 1):
         print("On the " + str(i) + "th level of the search tree")
         feature_to_add_at_this_level = []
         best_so_far_accuracy = 0
 
-        for k in range(num_features):
+        for k in range(1, crossvalidation.num_features + 1):
             # make sure to not consider already added features (i.e. do not add feature 1 if already in current_set_of_features)
             if k not in current_set_of_features:
-                acc = accuracy() 
+                # print(current_set_of_features)
+                # print("RUNNING CROSS VALIDATION W " + str(current_set_of_features) + " AND K " + str(k))
+                acc = crossvalidation.cross_validation_accuracy(crossvalidation.dataframe.copy(), current_set_of_features, k)
+                # acc = accuracy()
                 print("--consider adding the " + str(k) + "th feature with accuracy: " + str(acc))
+                # print(current_set_of_features)
                 
                 if acc > best_so_far_accuracy:
                     best_so_far_accuracy = acc
                     feature_to_add_at_this_level = k
+
+
+
         current_set_of_features.append(feature_to_add_at_this_level)
-        print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) + " to current set" )
+        print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) + " to current set WITH accuracy " + str(best_so_far_accuracy) )
+        print(current_set_of_features)
+
+
+def backwards_elimination():
+    print("test")
 
 
 feature_search()
